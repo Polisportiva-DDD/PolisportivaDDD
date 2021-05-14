8c066179dcc562be29bb87f27cb57c57ac3e99c2
Urwen1999 <giorgio.dinunzio@student.univaq.it>
2021-05-14 09:53:43 +0200

    Aggiunta classe CartadiCredito.php

---
+++ CartadiCredito.php
@@ -0,0 +1,105 @@
+<?php
+
+
+class CartadiCredito
+{
+    //attributes
+
+    private string $numero;
+    private string $nome_titolare;
+    private string $cognome_titolare;
+    private string $CVC;
+    private DateTime $scadenza;
+
+    //methods
+
+    /**
+     * Restituisce il numero della carta di credito
+     * @return string
+     */
+    public function getNumero(): string
+    {
+        return $this->numero;
+    }
+
+    /**
+     * Restituisce il nome del titolare della carta di credito
+     * @return string
+     */
+    public function getNomeTitolare(): string
+    {
+        return $this->nome_titolare;
+    }
+
+    /**
+     * Restituisce il cognome del titolare della carta di credito
+     * @return string
+     */
+    public function getCognomeTitolare(): string
+    {
+        return $this->cognome_titolare;
+    }
+
+    /**
+     * Restituisce il codice di sicurezza della carta di credito
+     * @return string
+     */
+    public function getCVC(): string
+    {
+        return $this->CVC;
+    }
+
+    /**
+     * Restituisce la data di scadenza della carta di credito
+     * @return DateTime
+     */
+    public function getScadenza(): DateTime
+    {
+        return $this->scadenza;
+    }
+
+    /**
+     * Imposta il numero della carta di credito
+     * @param string $numero
+     */
+    public function setNumero(string $numero): void
+    {
+        $this->numero = $numero;
+    }
+
+    /**
+     * Imposta il nome del titolare della carta di credito
+     * @param string $nome_titolare
+     */
+    public function setNomeTitolare(string $nome_titolare): void
+    {
+        $this->nome_titolare = $nome_titolare;
+    }
+
+    /**
+     * Imposta il cognome del titolare della carta di credito
+     * @param string $cognome_titolare
+     */
+    public function setCognomeTitolare(string $cognome_titolare): void
+    {
+        $this->cognome_titolare = $cognome_titolare;
+    }
+
+    /**
+     * Imposta il codice di sicurezza della carta di credito
+     * @param string $CVC
+     */
+    public function setCVC(string $CVC): void
+    {
+        $this->CVC = $CVC;
+    }
+
+    /**
+     * Imposta la data di scadenza della carta di credito
+     * @param DateTime $scadenza
+     */
+    public function setScadenza(DateTime $scadenza): void
+    {
+        $this->scadenza = $scadenza;
+    }
+}
