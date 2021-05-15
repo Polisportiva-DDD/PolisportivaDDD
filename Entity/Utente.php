6ca413d01f88d3faafd3d5ca35eb9bb4116c2da4
Urwen1999 <giorgio.dinunzio@student.univaq.it>
2021-05-15 16:18:48 +0200

    vediamo se ci riusciamo

--- Entity/Utente.php
+++ Entity/Utente.php
@@ -1,4 +1,7 @@
 <?php
+include "Campo.php";
+include "Gruppo.php";
+include "CartadiCredito.php";
 include "Recensione.php";
  class Utente{
 	private string $username;
@@ -6,19 +9,55 @@ include "Recensione.php";
 	private string $cognome;
 	private string $email;
 	private string $password;
-	private  DateTime $data;
+	private DateTime $data;
 	private float $mediaRecensioni;
-	private array  $recensioni=array() ;
+	private array  $recensioni=array();
+	private array $cartedicredito=array();
+	private array $gruppo=array();
 
+	 /**
+	  * @return array
+	  */
+	 public function getGruppo(): array
+	 {
+		 return $this->gruppo;
+	 }
 
-	  public function __construct(String $u,String $n,String $c,String $e,String $p,DateTime $d,array $arr){
-		$this->username=$u;
-        $this->nome=$n;
-        $this->cognome=$c;
-        $this->email=$e;
-		$this->password=$p;
-        $this->data=$d;
-        $this->recensioni=$arr;
+	 /**
+	  * @param array $gruppo
+	  */
+	 public function setGruppo(array $gruppo): void
+	 {
+		 $this->gruppo = $gruppo;
+	 }
+
+	 /**
+	  * @return array
+	  */
+	 public function getCartedicredito(): array
+	 {
+		 return $this->cartedicredito;
+	 }
+
+	 /**
+	  * @param array $cartedicredito
+	  */
+	 public function setCartedicredito(array $cartedicredito): void
+	 {
+		 $this->cartedicredito = $cartedicredito;
+	 }
+
+	  public function __construct(String $username,String $nome,
+								  String $cognome,String $email,
+								  String $password,DateTime $dataDiNascita,
+								  array $recensioni){
+		$this->username=$username;
+        $this->nome=$nome;
+        $this->cognome=$cognome;
+        $this->email=$email;
+		$this->password=$password;
+        $this->data=$dataDiNascita;
+        $this->recensioni=$recensioni;
     }
 
 	 /**
@@ -28,7 +67,7 @@ include "Recensione.php";
      public function getRecensioni():array{
 		return $this->recensioni;
 	}
-	
+
      /**
       * Restituisce l'username dell'utente
       * @return String
@@ -36,7 +75,7 @@ include "Recensione.php";
      public function getUsername():String{
 		return $this->username;
 	}
-	
+
 
      /**
       * Restituisce il nome dell'utente
@@ -94,7 +133,7 @@ include "Recensione.php";
      public function setUsername(String $u):void{
 		 $this->username=$u;
 	}
-	
+
 	/**
       * Imposta l'array delle recensioni
       * @param array arr
@@ -151,10 +190,6 @@ include "Recensione.php";
 		 $this->mediaRecensioni=$m;
 	}
 
-     /**
-      * Restituisce la media delle recensioni dell'utente.
-      * @return float
-      */
      public function calcolaMediaRecensioni():float{
 		 $totale=0;
 		 $c=0;
@@ -174,61 +209,157 @@ include "Recensione.php";
 		}
 	}
 
-     /**
-      * Aggiunge la recensione passata come parametro alla lista delle recensioni dell'utente. Restituisce true se
-      * l'operazione è andata a buon fine, false altrimenti.
-      * @return bool
-      */
-	public function aggiungiRecensione(Recensione $recensione): bool{
+	public function rimuoviGruppo(int $id):bool{
+     	foreach($this->gruppo as $chiave => $valore){
+     		if($id==$valore->getId()){
+				unset($this->gruppo[$chiave]);
+     			return true;
+			}
+		}
+     	return false;
+	}
 
-         try{
-             if ($recensione != null){
-                 array_push($this->recensioni, $recensione);
-                 return true;
-             }
-             else{
-                 return false;
-             }
-         }
-         catch (Exception $exception){
-             //Gestione dell'eccezione
-             return false;
-        }
+	public function aggiungiGruppo(Gruppo $gruppo):bool{
+			array_push($this->gruppo, $gruppo);
+     	return true;
+	}
+
+	 /**
+	  * Aggiunge la carta di credito passata come parametro alla lista delle carte di credito dell'utente.
+	  * Restituisce true se l'operazione è andata a buon fine, false altrimenti.
+	  * @param CartadiCredito $carta
+	  * @return bool
+	  */
+	public function aggiungiCarta(CartadiCredito $carta):bool
+	{
+		try
+		{
+			if($carta != null)
+			{
+				array_push($this->cartedicredito,$carta);
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		catch (Exception $exception){
+			//Gestione dell'eccezione
+			return false;
+		}
+
     }
 
-     /**
-      * Rimuove la recensione passata come parametro alla lista delle recensioni dell'utente. Restituisce true se
-      * l'operazione è andata a buon fine, false altrimenti.
-      * @return bool
-      */
-    public function rimuoviRecensione(int $idRecensione): bool{
-         $offset = 0;
-         $found = false;
-         try{
-             foreach($this->recensioni as $recensione)
-             {
-                 if($recensione->getId == $idRecensione) {
-                     //rimuovi la recensione trovata
-                     array_splice($this->recensioni, $offset, 1);
-                 }
-                 $offset++;
-             }
-             return $found;
-         }
-         catch(Exception $exception){
-             //Errore
-             return false;
-         }
-    }
+	 /**
+	  * Rimuove la carta di credito passata come parametro alla lista delle carte di credito dell'utente.
+	  * Restituisce true se l'operazione è andata a buon fine, false altrimenti.
+	  * @param CartadiCredito $carta
+	  * @return bool
+	  */
+	public function rimuoviCarta(CartadiCredito $carta):bool
+	{
+		try
+		{
+			$numero = $carta ->getNumero();
+			foreach($this->cartedicredito as $chiave => $valore)
+			{
+				if($numero==$valore->getNumero())
+				{
+					unset($this->cartedicredito[$chiave]);
+					return true;
+				}
+			}
+			return false;
+		}
+		catch(Exception $exception){
+			//Errore
+			return false;
+		}
 
+	}
 
+	 /**
+	  * Aggiunge la recensione passata come parametro alla lista delle recensioni dell'utente. Restituisce true se
+	  * l'operazione è andata a buon fine, false altrimenti.
+	  * @return bool
+	  */
+	 public function aggiungiRecensione(Recensione $recensione): bool{
+
+		 try{
+			 if ($recensione != null){
+				 array_push($this->recensioni, $recensione);
+				 return true;
+			 }
+			 else{
+				 return false;
+			 }
+		 }
+		 catch (Exception $exception){
+			 //Gestione dell'eccezione
+			 return false;
+		 }
+	 }
+
+	 /**
+	  * Rimuove la recensione passata come parametro alla lista delle recensioni dell'utente. Restituisce true se
+	  * l'operazione è andata a buon fine, false altrimenti.
+	  * @return bool
+	  */
+	 public function rimuoviRecensione(int $idRecensione): bool{
+		 $offset = 0;
+		 $found = false;
+		 try{
+			 foreach($this->recensioni as $recensione)
+			 {
+				 if($recensione->getId == $idRecensione) {
+					 //rimuovi la recensione trovata
+					 array_splice($this->recensioni, $offset, 1);
+				 }
+				 $offset++;
+			 }
+			 return $found;
+		 }
+		 catch(Exception $exception){
+			 //Errore
+			 return false;
+		 }
+	 }
 
 }
+
 $r1=new Recensione(1,2.4,"Natale","ciao",new DateTime("2011-01-01T15:03:01.012345Z"));
 $r2=new Recensione(1,4.4,"Natale","ciao",new DateTime("2011-01-01T15:03:01.012345Z"));
 $r3=4;
 $arr=array($r1,$r2,$r3);
 
+$carta1 = new CartadiCredito("0000111122223333","Giorgio","Di Nunzio","222",new DateTime('now'));
+$carta2 = new CartadiCredito("0000111122224444","luca","bo","333",new DateTime('now'));
+$carta3 = new CartadiCredito("0000111122225555","lorenzo","forse","444",new DateTime('now'));
+$carta4 = new CartadiCredito("0000111122226666","franco","ma si","555",new DateTime('now'));
+
 $u=new Utente("lor","lorenzo","Diella","ccc","pass",new DateTime("2012-01-01T15:03:01.012345Z"),$arr);
-print $u->calcolaMediaRecensioni();
+$arr=array($u);
+$c=new Campo("c5", 5,6,"Ciao",1.5);
+$g1 = new Gruppo("Ciao",1,10,11,"forse",new DateTime('now'),$arr,$u,$c);
+$g2 = new Gruppo("izo",1,10,11,"si",new DateTime('now'),$arr,$u,$c);
+$g3 = new Gruppo("Giorgio",1,10,11,"no",new DateTime('now'),$arr,$u,$c);
+$g4 = new Gruppo("Franco",1,10,11,"dino",new DateTime('now'),$arr,$u,$c);
+
+$u->aggiungiGruppo($g4);
+//print(count($u->getGruppo()));
+$g=array($g1,$g4,$g3);
+$u->setGruppo($g);
+//print(count($u->getGruppo()));
+
+
+$u->aggiungiCarta($carta1);
+$u->aggiungiCarta($carta2);
+$u->aggiungiCarta($carta3);
+$u->aggiungiCarta($carta4);
+$u->rimuoviCarta($carta2);
+//print(count($u->getCartedicredito()));
+
+//$u->setCartedicredito($carta);
+print(count($u->getCartedicredito()));
+//ciao
 ?>
