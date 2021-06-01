dfd85cf66f15cbf7c332875d4882b486ea761b18
Urwen1999 <giorgio.dinunzio@student.univaq.it>
2021-06-01 11:58:43 +0200

    Importazione di Smarty e aggiunta della classe StartSmarty.php

---
+++ Utility/StartSmarty.php
@@ -0,0 +1,16 @@
+<?php
+
+//caricamento delle librerie di Smarty
+require('Smarty/Smarty.class.php');
+
+class StartSmarty extends Smarty {
+    static function configuration(): Smarty
+    {
+        $smarty=new Smarty();
+        $smarty->template_dir='Smarty/templates/';
+        $smarty->compile_dir='Smarty/templates_c/';
+        $smarty->config_dir='Smarty/configs/';
+        $smarty->cache_dir='Smarty/cache/';
+        return $smarty;
+    }
+}
