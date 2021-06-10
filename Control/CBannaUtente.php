cafb753a6db02bcbda367d67a1e2d6698a6daff4
lorenzodiella <84022522+lorenzodiella@users.noreply.github.com>
2021-06-10 19:21:45 +0200

    Cfront,Cbanna

---
+++ Control/CBannaUtente.php
@@ -0,0 +1,85 @@
+<?php
+
+require_once '../Utility/autoload.php';
+require_once '../Foundation/config.inc.php';
+
+class CBannaUtente
+{
+
+
+
+    public function __construct(){}
+
+    /**
+     * Funzione che mostra il profilo dell'utente
+     * @throws SmartyException
+     */
+    public function visualizzaProfilo(){
+        $session = new USession();
+        $isAmministratore = $session->readValue('isAmministratore');
+        $pm = new FPersistentManager();
+        $view = new VUtente();
+        if($_POST['username']){
+            $username = $_POST['username'];
+            $utenteDaBannare = $pm->load($username,"FUtente");
+            if($utenteDaBannare!=null){
+                $session->setValue('utente', serialize($utenteDaBannare));
+
+                $nome=$utenteDaBannare->getNome();
+                $cognome=$utenteDaBannare->getCognome();
+                //$data=$utenteDaBannare->getDataDiNascita();
+                $eta =  $utenteDaBannare->getEta();
+                $pic64=$utenteDaBannare->getImmagine();
+                $type="";
+                $recensioni=$pm->loadRecensioniUtente($username);
+                $valutazioneMedia=round($utenteDaBannare->calcolaMediaRecensioni($recensioni));
+                $view->showProfiloUtenteRegistrato($username, $nome, $cognome, $eta, $valutazioneMedia,$recensioni,$pic64, $type,$isAmministratore);
+            }
+           else{
+               $view->showErrore($username,$isAmministratore );
+           }
+
+        }
+        //???
+        $view->showErrore( );
+
+    }
+
+    /**
+     *Funzione che mostra la pagina in cui si banna un utente
+     */
+    public function banna(){
+        $session = new USession();
+        $pm = new FPersistentManager();
+        $view = new VAmministratore();
+        $utente =unserialize($session->readValue('utente'));
+        $username=$utente->getUsername();
+        $nome=$utente->getNome();
+        $cognome=$utente->getCognome();
+        $eta=$utente->getEta();
+        $recensioni=$pm->loadRecensioniUtente($username);
+        $valutazioneMedia=round($utente->calcolaMediaRecensioni($recensioni));
+        $pic64=$utente->getImmagine();
+        $type="";
+        $view->showBannaUtente($username, $nome, $cognome, $eta, $valutazioneMedia,$pic64, $type);
+
+
+    }
+
+    public function inviaBan(){
+        $session = new USession();
+        $pm = new FPersistentManager();
+        if ($_POST['motivazione']){
+            $motivazione = $_POST['idCampo'];
+            $utente =unserialize($session->readValue('utente'));
+            $username=$utente->getUsername();
+            $pm->updateUtenteRegistrato($username,true,$motivazione);
+            //se va male l'update???
+            header('Location: /ProgettoWeb/Admin/home');
+
+        }
+
+    }
+
+
+}
---
+++ Control/CFrontController.php
@@ -0,0 +1,187 @@
+<?php
+
+//require_once 'StartSmarty.php';
+require_once '../Utility/autoload.php';
+require_once '../Foundation/config.inc.php';
+require_once '../Utility/USession.php';
+class CFrontController
+{
+
+
+
+
+    public function __constructor(){
+
+    }
+
+
+
+    public function run2($path){
+
+        $resource = explode('/', $path);
+
+        array_shift($resource);
+        print_r($resource);
+        if($resource[0] != ''){
+            $controller = "C" . $resource[0];
+            if(class_exists($controller)){
+                if(method_exists($controller,$resource[1])){
+                    $real_controller=new $controller();
+
+                }
+                else{
+                    header('HTTP/1.1 405 Method Not Allowed');
+                    exit;
+                }
+            }
+            else{
+                header('HTTP/1.1 404 Not Found');
+                exit;
+            }
+
+        }
+        if(count($resource)==3){
+            $params=$resource[2];
+            return $real_controller->$resource[1]($params);
+        }
+        $method=$resource[1];
+        print ($method);
+        return $real_controller->$method();
+    }
+
+
+
+
+
+   /* public function run ($path)
+    {
+
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        $resource = explode('/', $path);
+
+        array_shift($resource);
+        array_shift($resource);
+
+        if ($resource[0] != 'api') {
+
+            $controller = "C" . $resource[0];
+           // $dir = 'Controller';
+          //  $eledir = scandir($dir);
+            if(class_exists($controller)){
+                if(method_exists($controller,)){}
+            }
+
+            if (in_array($controller . ".php", $eledir)) {
+                if (isset($resource[1])) {
+                    $function = $resource[1];
+                    if (method_exists($controller, $function)) {
+
+                        $param = array();
+                        for ($i = 2; $i < count($resource); $i++) {
+                            $param[] = $resource[$i];
+                            $a = $i - 2;
+                        }
+                        $num = (count($param));
+                        if ($num == 0) $controller::$function();
+                        else if ($num == 1) $controller::$function($param[0]);
+                        else if ($num == 2) $controller::$function($param[0], $param[1]);
+                        //else if ($num == 3) $controller::$function($param[0], $param[1], $param[2]);
+                        //else if ($num == 4) $controller::$function($param[0], $param[1], $param[2], $param[3]);
+                        //else if ($num == 5) $controller::$function($param[0], $param[1], $param[2], $param[3], $param[4]);
+                        //else if ($num == 6) $controller::$function($param[0], $param[1], $param[2], $param[3], $param[4], $param[5]);
+
+                    }
+					else {
+						if (CUtente::isLogged()) {
+							$utente = unserialize($_SESSION['utente']);
+							if ($utente->getEmail() == 'admin@admin.com')
+								header('Location: /FillSpaceWEB/Admin/homepage');
+							else {
+								//$smarty = StartSmarty::configuration();
+								CRicerca::trasportiHome();
+							}
+						}
+						else {
+							//$smarty = StartSmarty::configuration();
+							CRicerca::trasportiHome();
+						}
+					}
+                }
+                else {
+                    if (CUtente::isLogged()) {
+                        $utente = unserialize($_SESSION['utente']);
+                        if ($utente->getEmail() == 'admin@admin.com')
+                            header('Location: /FillSpaceWEB/Admin/homepage');
+                        else {
+                            //$smarty = StartSmarty::configuration();
+                            CRicerca::trasportiHome();
+                        }
+                    }
+                    else {
+                        //$smarty = StartSmarty::configuration();
+                        CRicerca::trasportiHome();
+                    }
+                }
+            }
+                else {
+                    if (CUtente::isLogged()) {
+                        $utente = unserialize($_SESSION['utente']);
+                        if ($utente->getEmail() == 'admin@admin.com')
+                            header('Location: /FillSpaceWEB/Admin/homepage');
+                        else {
+                            //$smarty = StartSmarty::configuration();
+                            CRicerca::trasportiHome();
+                        }
+                    }
+                    else {
+                        //$smarty = StartSmarty::configuration();
+                        CRicerca::trasportiHome();
+                    }
+                }
+
+        } // piattaforma mobile
+        else {
+
+            if ($resource[1] == 'annuncio'){
+                $val = (int) $resource[2];
+                CGestioneMobile::dettagliAnnuncio($val);
+            }
+            elseif ($resource[1] == 'ads'){
+                array_shift($resource);
+                array_shift($resource);
+                CGestioneMobile::showAds($resource[0]);
+            }
+
+            elseif ($resource[1] == 'filtri'){
+                $val = json_decode(file_get_contents('php://input'), true);
+                print ("ciao");
+                CGestioneMobile::filtra($val);
+
+            }
+
+			elseif ($resource[1] == 'login') {
+				CGestioneMobile::loginToken();
+			}
+
+        }
+
+
+    }
+/*
+    private function VerCookie() {
+    	$cookieOk = false;
+		setcookie('cookie_test', 'verifica', time()+3600);
+			if (isset($_COOKIE["cookie_test"]))
+			{
+				$cookieOk = true;
+			}
+		return $cookieOk;
+	}
+*/
+
+}
+
+$c=new CFrontController();
+$c->run2("ProgettoWeb/gruppo/scegliData");
+
